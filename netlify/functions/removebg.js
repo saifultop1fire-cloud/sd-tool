8f3a0f8ac147947a3ef6d20843c5f0fcaf82d1f4
saifultop1fire-cloud <saifultop1fire@gmail.com>
2026-07-18 13:42:33 +0600

    Refactor removebg function to use JSON payload

--- netlify/functions/removebg.js
+++ netlify/functions/removebg.js
@@ -15,26 +15,57 @@ exports.handler = async (event, context) => {
     const { image } = JSON.parse(event.body);
     const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
 
-    const bodyParams = new URLSearchParams();
-    bodyParams.append("image_file_b64", base64Data);
-    bodyParams.append("size", "auto");
-    const bodyStr = bodyParams.toString();
+    const dataStr = JSON.stringify({
+      image_file_b64: base64Data,
+      size: "auto"
+    });
 
-    const responseData = await new Promise((resolve, reject) => {
+    const responseBuffer = await new Promise((resolve, reject) => {
       const options = {
         hostname: "api.remove.bg",
         path: "/v1.0/removebg",
         method: "POST",
         headers: {
           "X-Api-Key": "gUYnLj9cgpFzKE5HXhWKkDqD",
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Content-Length": Buffer.byteLength(bodyStr)
+          "Content-Type": "application/json",
+          "Content-Length": Buffer.byteLength(dataStr)
         }
       };
 
       const req = https.request(options, (res) => {
         if (res.statusCode !== 200) {
           let errText = "";
+          res.on("data", (chunk) => errText += chunk);
+          res.on("end", () => reject(new Error(`RemoveBg HTTP: ${res.statusCode} - ${errText}`)));
+          return;
+        }
+
+        const chunks = [];
+        res.on("data", (chunk) => chunks.push(chunk));
+        res.on("end", () => resolve(Buffer.concat(chunks)));
+      });
+
+      req.on("error", (e) => reject(e));
+      req.write(dataStr);
+      req.end();
+    });
+
+    const base64Output = responseBuffer.toString("base64");
+
+    return {
+      statusCode: 200,
+      headers: { ...headers, "Content-Type": "application/json" },
+      body: JSON.stringify({ image: `data:image/png;base64,${base64Output}` })
+    };
+
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};          let errText = "";
           res.on("data", (chunk) => errText += chunk);
           res.on("end", () => reject(new Error(`RemoveBg HTTP: ${res.statusCode} - ${errText}`)));
           return;
